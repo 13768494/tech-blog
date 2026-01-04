8bdd48139ca4346fe9950e7320a1b73ffea8925b
帅成马 <90511611+13768494@users.noreply.github.com>
2026-01-04 17:23:09 +0800

    1

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 // db.php - 数据库连接（请根据实际修改 host/user/pass/dbname）
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASS = "root";
+$DB_HOST = "数据库链接地址";
+$DB_USER = "数据库账户";
+$DB_PASS = "数据库密码";
 $DB_NAME = "tech_blog";
 
 $db = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
--- tech_blog.sql
+++ tech_blog.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : 5.7.26
- Source Server Type    : MySQL
- Source Server Version : 50726
- Source Host           : localhost:3306
- Source Schema         : tech_blog
-
- Target Server Type    : MySQL
- Target Server Version : 50726
- File Encoding         : 65001
-
- Date: 30/12/2025 21:17:41
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
